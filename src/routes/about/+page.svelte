7cbbcf997730ff7b03ebd78022feca38a9a49c29
sudo-self <sudo-self@proton.me>
2023-10-29 22:43:02 -0600

    Initial commit

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>The Creator</h1>
+
+	<p>
+		This is a <a href="https://kit.svelte.dev">A Content App</a> You can make your work easy to access and in a fresh style thats reliable and current
+	</p>
+
+	<pre>Jesse@JesseJesse.com</pre>
+
+	<p>
+		The page you're looking at is purely static HTML, with no client-side interactivity needed.
+		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
+		the devtools network panel and reloading.
+	</p>
+</div>
